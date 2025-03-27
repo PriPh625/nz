f4110259c90c5a6da048fc61ccf81d1116d37c8e
PriPh625 <philip99price@gmail.com>
2025-03-27 15:36:24 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+
+/*
+Script für die Neuseelandreise
+
+*/
+
+// Karte initialisieren
 var map = L.map('map').setView([-44.116667, 170.166667], 11);
+
+// Hintergrund definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+        // Markerzeichnen
         var marker = L.marker([-44.116667, 170.166667]).addTo(map);
+
+        //Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();

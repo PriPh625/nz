09b69d35ae5b83d91b2c8e1ebda2a3ad8c1540d7
PriPh625 <philip99price@gmail.com>
2025-03-27 15:39:10 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ Script für die Neuseelandreise
 */
 
 // Karte initialisieren
-var map = L.map('map').setView([-44.116667, 170.166667], 11);
+let map = L.map('map').setView([-44.116667, 170.166667], 11);
 
 // Hintergrund definieren
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-        // Markerzeichnen
-        var marker = L.marker([-44.116667, 170.166667]).addTo(map);
+ // Markerzeichnen
+      let marker = L.marker([-44.116667, 170.166667]).addTo(map);
 
-        //Popup definieren und öffnen
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+//Popup definieren und öffnen
+      marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
